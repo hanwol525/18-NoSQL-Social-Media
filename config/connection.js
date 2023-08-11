50e7e3883b96d467ac591a75914fa7366341b1ec
HanWol <hannahwolfson05@gmail.com>
2023-08-10 22:20:25 -0500

    oops

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 
 // add your connection string to connect
-connect('mongodb+srv://hannahwolfson05:p8SD9gdMyQk8ewE6@cluster0.qbbci2g.mongodb.net/mongoDBHW');
+connect('mongodb+srv://hannahwolfson05:p8SD9gdMyQk8ewE6@cluster0.qbbci2g.mongodb.net/usersAndPosts');
 
 module.exports = connection;
